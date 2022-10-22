24200a530911966c4d801905481e0a9e08ff1067
Maria Rafaela <m.rafaela.f@gmail.com>
2022-10-22 17:31:52 -0300

    Criação do Projeto

---
+++ controle-funcionarios/src/main/java/com/api/controlefuncionarios/ControleFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.api.controlefuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFuncionariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleFuncionariosApplication.class, args);
+    }
+
+}
---
+++ controle-funcionarios/src/test/java/com/api/controlefuncionarios/ControleFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.controlefuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFuncionariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
